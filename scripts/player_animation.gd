25512376aab52617db5015b8728f16a5c38a96ae
leorr <leonardoribeiro.2016@alunos.utfpr.edu.br>
2019-05-11 17:33:06 -0300

    2019/05/11 17:33 moved the animation stuff to its own script, seems

---
+++ scripts/player_animation.gd
@@ -0,0 +1,58 @@
+extends AnimatedSprite
+#	   2
+#	   |		facing
+#       1-- --1
+#	   |
+#	   3
+
+var facing = 1
+
+func is_idle():
+	if (Input.is_action_pressed("ui_right") ||Input.is_action_pressed("ui_left") ||Input.is_action_pressed("ui_up") ||Input.is_action_pressed("ui_down")):
+		return false
+	else:
+		return true
+
+func get_input():
+	if Input.is_action_pressed("ui_right"):
+		set_flip_h(false)
+		facing=1
+		play("walkhorizontal")
+		
+	elif Input.is_action_pressed("ui_left"):
+		set_flip_h(true)
+		facing=1
+		play("walkhorizontal")
+
+	elif (Input.is_action_pressed("ui_up") && !(Input.is_action_pressed("ui_left") && Input.is_action_pressed("ui_right"))):
+			facing=2
+			play("walkup")
+
+	elif (Input.is_action_pressed("ui_down") && !(Input.is_action_pressed("ui_left") && Input.is_action_pressed("ui_right"))):
+			facing=3
+			play("walkdown")
+	
+	if(is_idle()):
+		if (facing == 1):
+			if(Input.is_action_just_released("ui_right") || Input.is_action_just_released("ui_left")):
+				play("toidlehorizontal")
+			elif(frame == 4):
+				play("idlehorizontal")
+
+		elif (facing == 2):
+			if(Input.is_action_just_released("ui_up")):
+				play("toidleup")
+			elif(frame == 4):
+				play("idleup")
+		
+		elif (facing == 3):
+			if(Input.is_action_just_released("ui_down")):
+				play("toidledown")
+			elif(frame == 4):
+				play("idledown")
+	
+
+	
+
+func _process(delta):
+	get_input()
--- scripts/player_motion.gd
+++ scripts/player_motion.gd
@@ -3,26 +3,21 @@ extends KinematicBody2D
 var motion = Vector2()
 var speed = 600
 
+
 func get_input():
 	motion = Vector2()
 	if Input.is_action_pressed("ui_right"):
 		motion.x += 1
-		$AnimatedSprite.play("walkright")
-
+		
 	if Input.is_action_pressed("ui_left"):
 		motion.x -= 1
-		$AnimatedSprite.play("walkleft")
 
 	if Input.is_action_pressed("ui_up"):
-		motion.y -= 1			#shit is inverted idk why
-		if(motion.x == 0):
-			$AnimatedSprite.play("walkup")
+		motion.y -= 1
 
 	if Input.is_action_pressed("ui_down"):
 		motion.y += 1
-		if(motion.x == 0):
-			$AnimatedSprite.play("walkdown")
-	
+
 	motion= motion.normalized() * speed
 
 func _physics_process(delta):
