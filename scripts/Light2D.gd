e0c1249495ce31d7c7e5893b51df11973c3da4c6
leorr <leonardoribeiro.2016@alunos.utfpr.edu.br>
2019-06-09 06:51:33 -0300

    2019/06/09 06:51 redo the lamp, use only only radial light source

--- scripts/Light2D.gd
+++ scripts/Light2D.gd
@@ -6,8 +6,8 @@ extends Light2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_z_range_max(get_z_index()+10)
-	set_z_range_min(get_z_index()-10)
+	set_z_range_max(get_z_index()+20)
+	set_z_range_min(get_z_index()-20)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
