78924321a2cf554aba319a98fb2cad64160e3703
leorr <leonardoribeiro.2016@alunos.utfpr.edu.br>
2019-06-09 00:58:56 -0300

    2019/06/09 00:58 jesus i am so slow

---
+++ Light2D.gd
@@ -0,0 +1,13 @@
+extends Light2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_z_range_max(get_z_index()+10)
+	set_z_range_min(get_z_index()-10)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
