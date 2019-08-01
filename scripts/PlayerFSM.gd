8e1511541874bb405866f07b4c614291a4d965d2
leorr <leonardoribeiro.2016@alunos.utfpr.edu.br>
2019-08-01 17:42:14 -0300

    2019/08/01 17:42 all set up

--- scripts/PlayerFSM.gd
+++ scripts/PlayerFSM.gd
@@ -1,14 +1,15 @@
 extends "res://scripts/State_Machine.gd"
 
+var facing = 0 
+
 func _ready():
 	add_state("idle")
 	add_state("walking")
-	call_deferred("set_state",states.walking)
+	call_deferred("set_state",states.idle)
 
 func _state_logic(delta):
 	parent._handle_move_input()
 	parent._apply_movement()
-
 
 func _get_transition(delta):#transition is to return
 	match state:
@@ -21,13 +22,42 @@ func _get_transition(delta):#transition is to return
 			if parent.motion.x == 0 && parent.motion.y ==0 :
 				return states.idle
 			else:
+				if parent.motion.x !=0:
+					if parent.motion.x >0:
+						facing = 1
+					else:
+						facing = 3
+				else:
+					if parent.motion.y > 0:
+						facing = 0
+					else:
+						facing = 2
 				return states.walking
 
 func _enter_state(new_state,old_state):
 	match new_state:
 		states.idle:
-			parent.get_node("anim_player").play("idledown")
-		states.walking:
-			parent.get_node("anim_player").play("walkdown")
+			match facing:
+				0:
+					parent.get_node("anim_player").play("idledown")
+				1:
+					parent.get_node("anim_player").play("idlehorizontal")
+				3:
+					parent.get_node("anim_player").play("idlehorizontal")
+				2:
+					parent.get_node("anim_player").play("idleup")
 
+
+		states.walking:
+			match facing:
+				0:
+					parent.get_node("anim_player").play("walkdown")
+				1:
+					parent.get_node("anim_player").set_flip_h(false)
+					parent.get_node("anim_player").play("walkhorizontal")
+				3:
+					parent.get_node("anim_player").set_flip_h(true)
+					parent.get_node("anim_player").play("walkhorizontal")
+				2:
+					parent.get_node("anim_player").play("walkup")
 
